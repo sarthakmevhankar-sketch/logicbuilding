12a041cb3e674be97d8226dfe78b3109bf234d69
Sarthak Mevhankar <sarthakmevhankar@gmail.com>
2026-06-20 23:32:28 +0530

    Add files via upload

---
+++ Assignment_26/program4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+    int i=0;
+    int j=0;
+    
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            if(i==1 || i==iRow || j==1 || j==iCol)
+            {
+                printf("*\t");                
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+    printf("Enetr number of rows:");
+    scanf("%d",&iValue1);
+    printf("Enetr number of columns:");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
